6ad38547c17ff26ffe9ff0d876f58e0362d6cecf
MatthieuOlenine <you@example.com38001232@parisnanterre.fr>
2023-11-19 16:35:20 +0100

    comparer market

--- WaspBody.py
+++ WaspBody.py
@@ -1,22 +1,28 @@
 import WaspService
+from time import sleep
+ 
+RUNNING = True
+Service = WaspService.Service()         # Donne accès au service de TheWasp
 
-Service = WaspService.Service()
+while RUNNING:
 
-Service.AtributResponse()
-Service.AtributWallet()
+    Service.LogIn()                         # connexion necessaire pour mettre à jour les datas
+    Service.AtributResponse()               # charge les données du marché et les trie dans 'Watched' et 'Normed' en conservant la dernière itération
+    Service.CompareMarket()                 # compare les dernières données du market avec la dernière itération
+    Service.AtributWallet()                 # charge les donnée du portefeuille et localise le wallet avec les fonds : 'pointer'
 
-watched = Service.GetWatched()
-normed = Service.GetNormed()
-pointer = Service.GetPointer()
-spotwallet = Service.GetSpotWallet()
+    winner = Service.GetWinner()            # transfere le nom du marché avec la plus haute perf dans 'winner'
+    normed = Service.GetNormed()            # transfere les dernières données de 'Normed' dans 'normed'
+    pointer = Service.GetPointer()          # transfere les données de 'Pointer' dans 'pointer'
+    spotwallet = Service.GetSpotWallet()    # transfere les données de 'SpotWallet' dans 'spotwallet'
 
-print('\nMarket TheWasp is looking at :\n')
-for a in watched:
-    print(f"{watched[watched.index(a)][0]}: {watched[watched.index(a)][1]}")
-print('\ncurrent prices of devices\n')
-for b in normed:
-    print(f"{normed[normed.index(b)][0]}: {normed[normed.index(b)][1]} USDT")
-print('\nWallet pointed by TheWasp :\n')
-for c in spotwallet:
-    print(f"{spotwallet[spotwallet.index(c)][0]}: {spotwallet[spotwallet.index(c)][1]} -> {spotwallet[spotwallet.index(c)][2]} USDT")
-print(f"\nWallet to transfer -> {pointer[0]}: {pointer[1]} -> {pointer[2]} USDT\n")
+    print('\ncurrent prices of devices\n')
+    for b in normed:
+        print(f"{normed[normed.index(b)][0]}: {normed[normed.index(b)][1]} USDT")
+    print('\nWallet pointed by TheWasp :\n')
+    for c in spotwallet:
+        print(f"{spotwallet[spotwallet.index(c)][0]}: {spotwallet[spotwallet.index(c)][1]} -> {spotwallet[spotwallet.index(c)][2]} USDT")
+    print(f"\nWallet to transfer -> {pointer[0]}: {pointer[1]} -> {pointer[2]} USDT")
+    print(f"Destination wallet -> {winner[0]}: ↑ de {winner[1]*-1} pts\n________________________")
+
+    sleep(60)
--- WaspService.py
+++ WaspService.py
@@ -1,12 +1,12 @@
 import WaspDataAccount
-import requests, json
+import requests, json, copy
 
 class Service:
     def __init__(self) -> None:
-        self.Pointer = [0, 0, 0]
+        self.winner = ['', 0]
+        self.Pointer = ['', 0, 0]
         self.Normed = [['', 0], ['', 0], ['', 0], ['', 0]]
-        self.Watched = [['', 0], ['', 0], ['', 0], ['', 0], ['', 0], ['', 0]]
-        self.WatchedLess1 = [['', 0], ['', 0], ['', 0], ['', 0], ['', 0], ['', 0]]
+        self.NormedLess1 = [['', 0], ['', 0], ['', 0], ['', 0]]
         self.SpotWallet = [['', 0, 0], ['', 0, 0], ['', 0, 0], ['', 0, 0], ['', 0, 0]]
         self.Url = 'https://api1.binance.com'
         self.ApiCall = '/api/v3/ticker/price'
@@ -15,27 +15,28 @@ class Service:
         self.AccountInfo = self.AccountInstance.Login()
         self.BalanceInfo = self.AccountInfo['balances']
 
+    def LogIn(self):
+        self.AccountInfo = self.AccountInstance.Login()
+        self.BalanceInfo = self.AccountInfo['balances']
+
     def AtributResponse(self):
         # attribue les données du marchée dans 'response'
-        # trie les données à surveiller dans 'watched'
-        # trie les données à utiliser pour normer en USDT dans 'normed'
+        # trie les données à surveiller dans 'watched' et conserve la dernière itération de 'Watched' dans 'WatchedLess1'
+        # trie les données à utiliser pour normer en USDT dans 'normed' et conserve la dernière itération de 'Normed' dans 'NormedLess1'
+        self.NormedLess1 = copy.deepcopy(self.Normed)
         response = requests.get(self.Url + self.ApiCall, headers=self.Headers)
         response = json.loads(response.text)
-        iteration_n = 0
-        iteration_w = 0
+        iteration = 0
         for e in response:
             if e['symbol']=='ETHUSDT' or e['symbol']=='BNBUSDT' or e['symbol']=='XRPUSDT' or e['symbol']=='BTCUSDT':
-                self.Normed[iteration_n][0] = e['symbol']
-                self.Normed[iteration_n][1] = e['price']
-                iteration_n += 1
-            if e['symbol']=='ETHBTC' or e['symbol']=='BNBETH' or e['symbol']=='XRPBNB' or e['symbol']=='XRPBTC' or e['symbol']=='BNBBTC' or e['symbol']=='XRPETH':
-                self.Watched[iteration_w][0] = e['symbol']
-                self.Watched[iteration_w][1] = e['price']
-                iteration_w += 1
+                self.Normed[iteration][0] = e['symbol']
+                self.Normed[iteration][1] = e['price']
+                iteration += 1
 
     def AtributWallet(self):
         # norme les valeur du portefeuille spot avec 'normed' puis attribue le wallet avec le plus gros actif à 'pointed'
         SpotPlace = 0
+        self.Pointer = ['', 0, 0]
         for i in self.BalanceInfo:
             if i['asset']=='BNB' or i['asset']=='ETH' or i['asset']=='BTC' or i['asset']=='XRP' or i['asset']=='USDT':
                 if i['asset']=='USDT':
@@ -45,22 +46,30 @@ class Service:
                         if (self.Normed[self.Normed.index(k)][0])[:-4]==i['asset']:
                             normedvalue = float(i['free'])*float(self.Normed[self.Normed.index(k)][1])
                 if normedvalue > float(self.Pointer[2]):
-                    self.Pointer = [i['asset'], i['free'], normedvalue]
+                    self.Pointer = [i['asset'], float(i['free']), normedvalue]
                 self.SpotWallet[SpotPlace][0] = i['asset']
                 self.SpotWallet[SpotPlace][1] = i['free']
                 self.SpotWallet[SpotPlace][2] = normedvalue
                 SpotPlace += 1
         
     def CompareMarket(self):
-        print('à faire')
+        transit = []
+        self.winner = ['', 0]
+        for j in range(0, (len(self.Normed))):
+            transit.append([self.Normed[j][0], round(float(self.NormedLess1[j][1])-float(self.Normed[j][1]), 6)])
+        for l in range(0, len(transit)):
+            if self.winner[1] > transit[l][1]:
+                self.winner = transit[l]
+
+    def GetWinner(self):
+        if float(self.winner[1]) < 0:
+            return self.winner
+        else:
+            return ['USDT', -0.0]
 
     def GetPointer(self):
         # obtenir 'pointer' (actif le + important du portefeuille)
         return self.Pointer
-
-    def GetWatched(self):
-        # obtenir 'watched' (market surveillé par TheWasp)
-        return self.Watched
 
     def GetNormed(self):        
         # obtenir 'normed' (prix du market utilisés pour normer les valeurs du portefeuille)
